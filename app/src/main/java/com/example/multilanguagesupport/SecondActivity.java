e158ed4d339c262bc6e126542629e7d5b601654e
pallaviB65 <pallavibharti5@gmail.com>
2020-04-02 17:15:06 +0530

    created Locale -- @Ketki

--- app/src/main/java/com/example/multilanguagesupport/SecondActivity.java
+++ app/src/main/java/com/example/multilanguagesupport/SecondActivity.java
@@ -3,10 +3,13 @@ package com.example.multilanguagesupport;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.util.Locale;
+
 public class SecondActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Locale.getDefault().getDisplayLanguage();
         setContentView(R.layout.activity_second);
     }
 
