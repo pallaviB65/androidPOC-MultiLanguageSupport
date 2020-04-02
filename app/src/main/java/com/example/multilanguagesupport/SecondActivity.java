738538aa0cc3f81143e909e745c7e70abf0f884e
pallaviB65 <pallavibharti5@gmail.com>
2020-04-02 18:52:52 +0530

    comitting locale changes -- @Ketki

--- app/src/main/java/com/example/multilanguagesupport/SecondActivity.java
+++ app/src/main/java/com/example/multilanguagesupport/SecondActivity.java
@@ -1,15 +1,23 @@
 package com.example.multilanguagesupport;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.Locale;
 
 public class SecondActivity extends Activity {
+    String myLocale;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Locale.getDefault().getLanguage();
+        Intent intent = getIntent();
+       /* String locale = intent.getStringExtra("locale");
+        Locale.setDefault(locale);*/
+        myLocale = Locale.getDefault().getLanguage();
+        Locale locale = new Locale(myLocale);
+        Locale.setDefault(locale);
+
         setContentView(R.layout.activity_second);
     }
 
