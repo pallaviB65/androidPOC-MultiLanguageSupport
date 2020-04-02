23f5c569c9ff961cae3800385dcb9dbabbb6eff7
pallaviB65 <pallavibharti5@gmail.com>
2020-04-02 11:07:33 +0530

    committing the second page changes for the POC -- @Ketki

--- app/src/main/java/com/example/multilanguagesupport/MainActivity.java
+++ app/src/main/java/com/example/multilanguagesupport/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.multilanguagesupport;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.loginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent myIntent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(myIntent);
+            }
+        });
 
         //getActionBar().hide();
     }
