d16f4055e8f37cfc27018b5b5dfc46095e9644e0
pallaviB65 <pallavibharti5@gmail.com>
2020-04-03 17:42:25 +0530

    made locale changes -- @Ketki

--- app/src/main/java/com/example/multilanguagesupport/MainActivity.java
+++ app/src/main/java/com/example/multilanguagesupport/MainActivity.java
@@ -1,27 +1,33 @@
 package com.example.multilanguagesupport;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import java.util.Locale;
 
 public class MainActivity extends Activity {
     Button button;
-    String myLocale;
-    Locale locale;
+    String localeName;
+    String currentLanguage = "en", currentLang;
+    Locale myLocale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        myLocale = Locale.getDefault().getLanguage();
-        locale = new Locale(myLocale);
-        Locale.setDefault(locale);
-
         setContentView(R.layout.activity_main);
+
+        localeName = Locale.getDefault().getLanguage();
+        setLocale(localeName);
+        /*locale = new Locale(myLocale);
+        Locale.setDefault(locale);*/
         button = findViewById(R.id.loginButton);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -33,5 +39,21 @@ public class MainActivity extends Activity {
                 startActivity(myIntent);
             }
         });
+    }
+
+    public void setLocale(String localeName) {
+        if (!localeName.equals(currentLanguage)) {
+            myLocale = new Locale(localeName);
+            Resources res = getResources();
+            DisplayMetrics dm = res.getDisplayMetrics();
+            Configuration conf = res.getConfiguration();
+            conf.locale = myLocale;
+            res.updateConfiguration(conf, dm);
+            Intent refresh = new Intent(this, MainActivity.class);
+            refresh.putExtra(currentLang, localeName);
+            startActivity(refresh);
+        } else {
+            Toast.makeText(MainActivity.this, "Language already selected!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
